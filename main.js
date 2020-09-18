af74a0dae3b0ed796696f4e24e264220226aec61
gilsonmneto <gilsonmneto@yahoo.com.br>
2020-09-18 00:23:03 -0300

    file and dev menu itens

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow, Menu, globalShortcut } = require("electron");
 //Set enviroment
 process.env.NODE_ENV = "development";
 
-const isDEV = process.env.NODE_ENV !== "production" ? true : false;
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
 const isMac = process.platform === "darwin" ? true : false;
 
 let mainWindow;
@@ -14,7 +14,7 @@ function createMainWindow() {
     width: 500,
     height: 600,
     icon: `${__dirname}/assets/icons/Icon_256x256.png`,
-    resizable: isDEV ? true : false,
+    resizable: isDev ? true : false,
     backgroundColor: "white",
   });
 
@@ -39,15 +39,21 @@ app.on("ready", () => {
 const menu = [
   ...(isMac ? [{ role: "appMenu" }] : []),
   {
-    label: "Arquivo",
-    submenu: [
-      {
-        label: "Sair",
-        accelerator: "CmdOrCtrl+W",
-        click: () => app.quit(),
-      },
-    ],
+    role: "fileMenu",
   },
+  ...(isDev
+    ? [
+        {
+          label: "Developer",
+          submenu: [
+            { role: "reload" },
+            { role: "forcereload" },
+            { type: "separator" },
+            { role: "toggledevtools" },
+          ],
+        },
+      ]
+    : []),
 ];
 
 app.on("window-all-closed", () => {
